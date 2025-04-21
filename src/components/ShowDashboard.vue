c34456e39e31be7c79caf7e089b7042929be1f4b
Orestis Molopodis <orestis@orestis.nl>
2025-04-21 12:34:05 +0200

    ⚡️ Improves UX with lazy loading and transition

--- src/components/ShowDashboard.vue
+++ src/components/ShowDashboard.vue
@@ -2,6 +2,11 @@
 import { useShowStore } from '../stores/showStore';
 
 const showStore = useShowStore();
+
+function handleImageLoad(event: Event) {
+  const target = event.target as HTMLImageElement;
+  target.classList.add('show-card__image--loaded');
+}
 </script>
 
 <template>
@@ -10,9 +15,20 @@ const showStore = useShowStore();
     <div v-for="(shows, genre) in showStore.showsByGenre" :key="genre">
       <h2>{{ genre }}</h2>
       <div class="show-list">
-        <router-link v-for="show in shows" :key="show.id" :to="`/show/${show.id}`" class="show-card">
+        <router-link
+          v-for="show in shows"
+          :key="show.id"
+          :to="`/show/${show.id}`"
+          class="show-card"
+        >
           <h3>{{ show.name }}</h3>
-          <img :src="show.image.medium" :alt="`Image of ${show.name}`" class="show-card__image" />
+          <img
+            :src="show.image.medium"
+            :alt="`Image of ${show.name}`"
+            class="show-card__image"
+            loading="lazy"
+            @load="handleImageLoad"
+          />
         </router-link>
       </div>
     </div>
@@ -33,7 +49,17 @@ const showStore = useShowStore();
 .show-card__image {
   width: 200px;
   height: 300px;
+  border: 3px solid transparent;
   object-fit: cover;
+  opacity: 0;
+  transition: opacity 0.2s ease-in;
+}
+.show-card__image--loaded {
+  opacity: 1;
+}
+.show-card:hover .show-card__image {
+  border-color: #fff;
+  transition: border-color 0.2s ease-in;
 }
 @media (min-width: 480px) {
   .show-card {
