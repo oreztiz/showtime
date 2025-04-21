d926ebf5aa315c3aee97ada1e43d4ec2ffa97e8a
Orestis Molopodis <orestis@orestis.nl>
2025-04-21 10:08:33 +0200

    🔧 Adds support for linting/formatting

---
+++ eslint.config.ts
@@ -0,0 +1,9 @@
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  skipFormatting,
+)
