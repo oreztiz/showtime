a6c1c1288be4ab79e57aa0c3a94fd609bf96db74
Orestis Molopodis <orestis@orestis.nl>
2025-04-21 10:47:19 +0200

    💄 Adds initial show dashboard

---
+++ src/components/ShowDashboard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useShowStore } from '../stores/showStore';
+
+const showStore = useShowStore();
+</script>
+
+<template>
+  <div>
+    <h1>TV Show Dashboard</h1>
+    <div v-for="show in showStore.shows" :key="show.id">
+      <router-link :to="`/show/${show.id}`">
+        <h2>{{ show.name }}</h2>
+        <img :src="show.image?.medium" :alt="`Image of ${show.name}`" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
